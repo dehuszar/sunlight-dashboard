35904e892c6b7cc0b16f098b70d24a7ef86e67db
Jim <blueeyeguy.jim@gmail.com>
2014-04-23 11:59:25 -0500

    Split out tasks in server.  Allows for id keys.  For example, /api/legislators/J000296 now sends back bioguide_id=J000296 in the request query

--- api/server.js
+++ api/server.js
@@ -1,50 +1,19 @@
 // TODO
 // ==================================
-// account for single request by ID
-
+// I'm sure there more here to do
 
 
 var express = 		require('express'),
+	request = 		require('request'),
 	http = 			require('http'),
 	querystring = 	require('querystring'),
-	request = 		require('request'),
 	app = 			express();
 
 
-app.get('/api/*', function(req, res) {
 
-	var params = 	req.params[0].split('/'),
-		endpoint = 	params[0],
-		id = 		params[1],
-		query;
-
-
-	// Allow front end to read from this endpoint (CORS)
-	res.setHeader('Access-Control-Allow-Origin', 'http://0.0.0.0:4200');
-
-
-	// add id to query
-	if(id) req.query.bioguide_id = id;
-
-	query = querystring.stringify(req.query);
-
-	
-	// Options for the request
-	var options = {
-		url: 'https://congress.api.sunlightfoundation.com/' + endpoint,
-		method: 'GET',
-		headers: {
-			'content-type': 'application/json; charset=UTF-8',
-			'X-APIKEY': '66603c029b1b49428da28d6a783f795e'
-		}
-	};
-
-
-	// Add query to request url
-	if(query) options.url = options.url  + '?' + query;
-	
-	console.log('Request from - ' + options.url);
-
+// Makes request based on options
+// ========================================================
+var makeRequest = function(res, endpoint, options){
 
 	// Triggers upon a response 
 	function callback(error, response, body) {
@@ -59,19 +28,73 @@ app.get('/api/*', function(req, res) {
 			// { 'legislators': [{ 'key': value }] }
 			responseData[endpoint] = data.results;
 
+			// Allow front end to read from this endpoint (CORS)
+			res.setHeader('Access-Control-Allow-Origin', 'http://0.0.0.0:4200');
+
 			// Send back response!
 			res.send(JSON.stringify(responseData));
 
 		}
 	}
 
+	console.log(' - Requesting: ' + options.url);
 	// Make the request
 	request(options, callback);
+};
+
+
+
+
+// Return Sunlight Foundation options
+// ========================================================
+var createOptions = function(req, res, idKey){
+	var params = 	req.path.split('/'),
+		endpoint = 	params[2],
+		id = 		params[3],
+		query;
+
+	// add id to query
+	if(id && idKey) req.query[idKey] = id;
+
+	query = querystring.stringify(req.query);
+
+	// Options for the request
+	var options = {
+		url: 'https://congress.api.sunlightfoundation.com/' + endpoint,
+		method: 'GET',
+		headers: {
+			'content-type': 'application/json; charset=UTF-8',
+			'X-APIKEY': '66603c029b1b49428da28d6a783f795e'
+		}
+	};
+
+	// Add query to request url
+	if(query) options.url = options.url  + '?' + query;
+
+	return options;
+}
+
+
+
+
+// ROUTES
+// ========================================================
+app.get('/api/legislators/*', function(req, res) {
+	makeRequest(res, 'legislators', createOptions(req, res, 'bioguide_id'));
+});
+
+
+// Wildcard route
+app.get('/api/*', function(req, res) {
+	var endpoint = req.path.split('/')[2];
+	makeRequest(res, endpoint, createOptions(req, res));
 });
 
 
 
+
 // Set up port
+// ========================================================
 var server = app.listen(3000, function() {
 	console.log(' - Listen to port ' + server.address().port);
 });
